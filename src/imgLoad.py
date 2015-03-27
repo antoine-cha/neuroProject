1edd844b4a177bd72b918eecd0fbcbb37a3598f0
Stephane Collot <stephane.collot@gmail.com>
2015-03-27 19:37:29 -0400

    creation of the patch list

--- src/imgLoad.py
+++ src/imgLoad.py
@@ -6,15 +6,17 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from random import randint
 
 print("Loading images from: " + IMGSET_PATH)
 
+nbrPatch = 100
+sizePatch = 20
 
 def subArray(arr, x, y):
-  size = 20
-  subArr = np.empty([size, size])
-  for i in range(size):
-    for j in range(size):
+  subArr = np.empty([sizePatch, sizePatch])
+  for i in range(sizePatch):
+    for j in range(sizePatch):
       subArr[i,j] = arr[i+x,j+y]
    
   return subArr
@@ -22,13 +24,19 @@ def subArray(arr, x, y):
 
 def rgb2gray(rgb):
   return np.dot(rgb[...,:3], [0.299, 0.587, 0.144])
-    
+
+
+patches = []
 os.chdir(IMGSET_PATH)
 for file in glob.glob("*.tif"):
   print("File "+file)
   img = plt.imread(IMGSET_PATH+file)
   gray = rgb2gray(img)
-  print(subArray(gray,1,1))
+ 
+  for i in range(nbrPatch):
+    x = randint(0,gray.shape[0]-sizePatch)
+    y = randint(0,gray.shape[1]-sizePatch)
+    patches.append(subArray(gray,x,y))
   
     
 
