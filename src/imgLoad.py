efabf8800a87e93d1f1e7e099e946bb19457ee1f
Stephane Collot <stephane.collot@gmail.com>
2015-03-27 18:44:43 -0400

    correction path, + first loop

--- src/imgLoad.py
+++ src/imgLoad.py
@@ -1,8 +1,16 @@
 # Loading images
+
 from conf import *
+import glob
+import os
 
 
 print("Loading images from: " + IMGSET_PATH)
+
+os.chdir(IMGSET_PATH)
+for file in glob.glob("*.tif"):
+  with open(IMGSET_PATH+file, 'r') as content_file:
+    print("File "+file)
 
 
 
