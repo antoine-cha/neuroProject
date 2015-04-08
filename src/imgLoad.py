01856d249cd7477c56e90c3e777a054e74203aaf
Stephane Collot <stephane.collot@gmail.com>
2015-04-08 18:51:40 -0400

    remove useless change directory + if main

--- src/imgLoad.py
+++ src/imgLoad.py
@@ -10,10 +10,7 @@ from random import randint
 
 print("Loading images from: " + IMGSET_PATH)
 
-nbrPatch = 100
-sizePatch = 20
-
-def subArray(arr, x, y):
+def subArray(arr, x, y, sizePatch):
   subArr = np.empty([sizePatch, sizePatch])
   for i in range(sizePatch):
     for j in range(sizePatch):
@@ -24,26 +21,27 @@ def subArray(arr, x, y):
 def rgb2gray(rgb):
   return np.dot(rgb[...,:3], [0.299, 0.587, 0.144])
 
+if __name__ == "__main__":
+  nbrPatch = 100 # Number of patch per image
+  sizePatch = 20 # Size in pixel of a patch
 
-patches = []
-owd = os.getcwd()
-os.chdir(IMGSET_PATH)
-for file in glob.glob("*.tif"):
-  print("File "+file)
-  img = plt.imread(IMGSET_PATH+file)
-  gray = rgb2gray(img)
- 
-  for i in range(nbrPatch):
-    x = randint(0,gray.shape[0]-sizePatch)
-    y = randint(0,gray.shape[1]-sizePatch)
-    patches.append(subArray(gray,x,y))
+  patches = []
+  for file in glob.glob(IMGSET_PATH+"*.tif"):
+    print("File "+file)
+    img = plt.imread(IMGSET_PATH+file)
+    gray = rgb2gray(img)
+   
+    for i in range(nbrPatch):
+      x = randint(0,gray.shape[0]-sizePatch)
+      y = randint(0,gray.shape[1]-sizePatch)
+      patches.append(subArray(gray,x,y,sizePatch))
   
+  #Shuffle patches:
     
-
-# Save the patches in a file
-# into patches.npy
-os.chdir(owd)
-np.save(PATCHES_PATH, patches)
+  
+  # Save the patches in a file
+  # into patches.npy
+  np.save(PATCHES_PATH, patches)
 
 
 
