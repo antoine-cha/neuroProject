b5f3a71c99c4c5ba6cb3f3a53a8790e30da7b5a5
antoine-cha <antoine.chassang0@gmail.com>
2015-04-08 16:13:40 -0400

    added visualization of feature vectors

--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 # Define the model used in the paper
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Model:
@@ -37,7 +38,8 @@ class Model:
         yw = np.dot(np.transpose(y), self.w)  # Y^T * W
         a = np.sum(yw)
         # equation [S4] in supplementary material
-        logC = sum([yw[k] * np.dot(self.b[k, :, :], np.transpose(self.b[k, :, :]))
+        b_t = np.transpose(self.b, (0, 2, 1))
+        logC = sum([yw[k] * np.dot(self.b[k, :, :], b_t[k, :, :])
                     for k in range(yw.shape[0])])
         # x^T * exp(-log C) * x
         b = np.dot(np.transpose(x), np.dot(np.exp(-logC), x))
@@ -45,3 +47,24 @@ class Model:
         c = -sum(np.abs(y))
 
         return -0.5 * (a + b + c)
+
+    def show_weight(self):
+        """
+        Display the feature vectors as black and white images
+        """
+        n = self.b.shape[0]
+        p = n//10 + 1
+        if (n%10==0):
+            p -= 1
+        f, axarr = plt.subplots(p, min(10, n))
+        for i in range(n):
+            axarr[i//10][i%10].matshow(b[i, :, :], interpolation='none', cmap='Greys')
+        plt.show()
+        return 0
+
+
+if __name__ == "__main__":
+    w = np.random.rand(1, 2)
+    b = np.random.rand(30, 20, 20)
+    mod = Model(w, b)
+    mod.show_weight()
